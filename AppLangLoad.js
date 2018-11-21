598d114e2c9b7e65ead560b974121da838d35e2b
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-11-21 16:30:41 -0500

    Update AppLangLoad.js

--- AppLangLoad.js
+++ AppLangLoad.js
@@ -1,3 +1,10 @@
 function getCode(file) {
 compile(readFile(file));
 }
+
+function init() {
+getCode("lib/text.applang");
+getCode("lib/batch.applang");
+getCode("lib/files.applang");
+getCode("lib/node.applang");
+}
